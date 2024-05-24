590300438df3058898e57ab8f086982295cf6919
mfauzi2020 <aweeawee72@gmail.com>
2024-05-24 20:08:31 +0700

    add something in  java script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,21 @@
 // ================== Java Script Luas Segitiga ==============//
-
-const alas = document.getElementById("alas");
-const tinggi = document.getElementById("tinggi");
-const hitung = document.getElementById("hitung");
-const output = document.getElementById("output");
-hitung.addEventListener("click", function () {
-  let a = alas.value;
-  let t = tinggi.value;
-  let l = 0.5 * a * t;
-  output.innerHTML = `Luas Segitiga dari alas ${a} cm dan tinggi ${t} cm adalah ${l} cm2`;
-});
+function iniKosong() {
+  const alas = document.getElementById("alas");
+  const tinggi = document.getElementById("tinggi");
+  const hitung = document.getElementById("hitung");
+  const output = document.getElementById("output");
+  hitung.addEventListener("click", function () {
+    let a = alas.value;
+    let t = tinggi.value;
+    let l = 0.5 * a * t;
+    output.innerHTML = `Luas Segitiga dari alas ${a} cm dan tinggi ${t} cm adalah ${l} cm2`;
+  });
+  if (document.getElementById("alas", "tinggi", ("hitung".value = "")));
+  {
+    output.innerHTML = "";
+  }
+  return;
+}
 
 // Validasi Form Luas Segitiga
 function iniKosong() {
@@ -24,6 +30,7 @@ function reset_luas() {
   document.getElementById("alas").value = "";
   document.getElementById("tinggi").value = "";
   document.getElementById("output").value = "";
+  output.innerHTML = "";
 }
 
 // End Java Script Hitung Luas Segitiga -------------
@@ -33,13 +40,16 @@ function keliling() {
   let s1 = document.getElementById("s1").value;
   let s2 = document.getElementById("s2").value;
   let s3 = document.getElementById("s3").value;
-  let jumlah = parseInt(s1) + parseInt(s2) + parseInt(s3);
   let hasil = document.getElementById("hasil");
+  let jumlah = parseInt(s1) + parseInt(s2) + parseInt(s3);
   hasil.innerHTML = `Hasil dari seluruh sisi atau keliling segitiga adalah ${jumlah} cm2`;
 
   // Validasi Form Keliling
-  if (document.getElementById("s1", "s2", "s3").value == "") {
+  if (document.getElementById("s1", "s2", "s3", "hasil").value == "") {
     alert("Masukkan nilai di field, Jangan Kosong !");
+  } else document.getElementById("s1", "s2", "s3", "hasil").value = "";
+  {
+    hasil.innerHTML = "";
   }
   return;
 }
@@ -48,6 +58,7 @@ function reset_keliling() {
   document.getElementById("s1").value = "";
   document.getElementById("s2").value = "";
   document.getElementById("s3").value = "";
+  hasil.innerHTML = "";
 }
 
 // End Java Script Hitung Keliling Segitiga -----------
